1473c3831dbcc5f8f9d9d91de30bca1d56ece913
kishinoa <109147221+kishinoa@users.noreply.github.com>
2025-06-08 23:02:48 +0900

    merge : 브랜치 최신화

--- src/main/java/xyz/catequest/spring/domain/question/controller/QuestionController.java
+++ src/main/java/xyz/catequest/spring/domain/question/controller/QuestionController.java
@@ -1,8 +1,9 @@
 package xyz.catequest.spring.domain.question.controller;
 
 import ch.qos.logback.core.model.Model;
-import java.math.BigInteger;
-import java.util.List;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ListIterator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,23 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 import xyz.catequest.spring.domain.question.entity.Question;
 import xyz.catequest.spring.domain.question.service.QuestionService;
 
+import java.math.BigInteger;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class QuestionController {
-  private final QuestionService questionService;
+    private final QuestionService questionService;
 
-  @GetMapping("/123")
-  public BigInteger questionMethod1(@RequestParam BigInteger num) {
-    return questionService.questionServiceMethod(num);
-  }
+//    @GetMapping("/1")
+//    public void questionMethod2(@RequestParam String que) {
+//        questionService.createQuestion(que);
+//    }
 
-  @GetMapping("/1")
-  public void questionMethod2(@RequestParam String que) {
-    questionService.createQuestion(que);
-  }
+    @GetMapping("/12")
+    public List<Question> questionMethod3(@RequestParam Long id, Model model) {
+        return questionService.findQuestionById(id);
+    }
 
-  @GetMapping("/")
-  public List<Question> questionMethod3(@RequestParam Long id, Model model) {
-    return questionService.findQuestionById(id);
-  }
+    @GetMapping("/")
+    public List<Question> questionMethod4(@RequestParam String category, Long category_in_id) {
+        return questionService.findAll(category, category_in_id);
+    }
 }
--- src/main/java/xyz/catequest/spring/domain/question/entity/Question.java
+++ src/main/java/xyz/catequest/spring/domain/question/entity/Question.java
@@ -10,13 +10,24 @@ import lombok.NoArgsConstructor;
 @Table(name = "questions")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Question {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column private String question;
+    @Column
+    private String question;
 
-  public Question(String question) {
-    this.question = question;
-  }
+    @Column
+    private String category;
+
+    @Column
+    private Long category_in_id;
+
+    public Question(String question, String category, Long category_in_id)
+    {
+        this.question = question;
+        this.category = category;
+        this.category_in_id = category_in_id;
+    }
+
 }
