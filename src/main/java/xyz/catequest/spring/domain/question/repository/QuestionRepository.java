18edce00638869db55d76afb6d52590928d0f08e
kishinoa <109147221+kishinoa@users.noreply.github.com>
2025-06-08 23:17:38 +0900

    merge : 브랜치 최신화

--- src/main/java/xyz/catequest/spring/domain/question/repository/QuestionRepository.java
+++ src/main/java/xyz/catequest/spring/domain/question/repository/QuestionRepository.java
@@ -1,11 +1,13 @@
 package xyz.catequest.spring.domain.question.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import xyz.catequest.spring.domain.question.entity.Question;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-  List<Question> findQuestionById(Long id);
+    List<Question> findQuestionById(Long id);
+    List<Question> findByCategoryAndCategory_in_id(String category, Long category_in_id);
 }
