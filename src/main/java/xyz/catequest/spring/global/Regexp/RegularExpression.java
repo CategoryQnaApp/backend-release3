c0bc4ef63960b66e519e5b97883e2e10613c45f4
SemiProgramer <85469908+SemiProgramer@users.noreply.github.com>
2025-07-15 02:06:22 +0900

    fix(security):보안이슈가 있는 정규식 변수명 수정 - PASSWORD -> PASSWORD_ROLE - EMAIL -> EMAIL_ROLE

--- src/main/java/xyz/catequest/spring/global/Regexp/RegularExpression.java
+++ src/main/java/xyz/catequest/spring/global/Regexp/RegularExpression.java
@@ -1,6 +1,6 @@
 package xyz.catequest.spring.global.Regexp;
 
 public interface RegularExpression {
-  String EMAIL = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-  String PASSWORD = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$";
+  String EMAIL_ROLE = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+  String PASSWORD_ROLE = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$";
 }
