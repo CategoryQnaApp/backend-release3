c75a1e291a68e28febd147b656ff3d2fe98ff775
SemiProgramer <85469908+SemiProgramer@users.noreply.github.com>
2025-07-15 22:18:18 +0900

    fix: ErrorMessage 메세지 수정

--- src/main/java/xyz/catequest/spring/global/Regexp/RegularExpression.java
+++ src/main/java/xyz/catequest/spring/global/Regexp/RegularExpression.java
@@ -4,5 +4,5 @@ public interface RegularExpression {
   String EMAIL_ROLE =
       "^(?![.])(?!.*[.]{2})[A-Za-z0-9+_.-]+(?<![.])@"
           + "[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.[A-Za-z]{2,}$";
-  String PWD_ROLE = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$";
+  String PASSWORDS_ROLE = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$";
 }
