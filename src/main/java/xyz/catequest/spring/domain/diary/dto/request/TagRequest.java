324575cba4a0baf8b354dccc5e92e7d992f31342
mixedsider <dmstjd0812@gmail.com>
2025-08-01 08:46:42 +0900

    fix: 각 태그 제약조건 추가
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/catequest/spring/domain/diary/dto/request/TagRequest.java
+++ src/main/java/xyz/catequest/spring/domain/diary/dto/request/TagRequest.java
@@ -1,7 +1,9 @@
 package xyz.catequest.spring.domain.diary.dto.request;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Getter;
@@ -13,5 +15,5 @@ public class TagRequest {
   @Size(max = 5)
   @Valid
   @NotEmpty
-  private final List<String> tagList;
+  private final List<@NotBlank @Size(min = 1, max = 20) String> tagList;
 }
