b272fcc2046a3a9315f64d629e369b46c6d99944
mixedsider <dmstjd0812@gmail.com>
2025-08-01 08:47:53 +0900

    style: 코드 스타일 변경
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/catequest/spring/domain/diary/dto/request/TagRequest.java
+++ src/main/java/xyz/catequest/spring/domain/diary/dto/request/TagRequest.java
@@ -3,7 +3,6 @@ package xyz.catequest.spring.domain.diary.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Getter;
