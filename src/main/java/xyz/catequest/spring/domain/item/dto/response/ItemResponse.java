7d66b3fa92d23633c3c03db435dc2c13979c8516
mixedsider <dmstjd0812@gmail.com>
2025-08-27 21:17:38 +0900

    refactor: valid 추가, null 검사 추가 - request 에 valid 추가 - response 에 null 검사 추가
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/catequest/spring/domain/item/dto/response/ItemResponse.java
+++ src/main/java/xyz/catequest/spring/domain/item/dto/response/ItemResponse.java
@@ -1,10 +1,12 @@
 package xyz.catequest.spring.domain.item.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import xyz.catequest.spring.domain.item.entity.Item;
 import xyz.catequest.spring.domain.item.enums.ItemType;
+import xyz.catequest.spring.global.enums.ErrorMessage;
 
 @Getter
 @RequiredArgsConstructor(staticName = "of")
@@ -17,7 +19,8 @@ public class ItemResponse {
   private final LocalDateTime createdAt;
 
   public static ItemResponse from(Item item) {
-    return new ItemResponse(
+    Objects.requireNonNull(item, ErrorMessage.NOT_FOUND_ITEM.getMessage());
+    return ItemResponse.of(
         item.getId(),
         item.getName(),
         item.getDescription(),
