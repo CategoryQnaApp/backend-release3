ea106f004a920bb11ae138848d6c8f359325d801
mixedsider <dmstjd0812@gmail.com>
2025-08-29 20:23:00 +0900

    fix: BaseEntity 의 createdAt, updatedAt 이 적용이 되지 않던 점 수정
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

---
+++ src/main/java/xyz/catequest/spring/config/data/AuditingEntityListenerConfig.java
@@ -0,0 +1,8 @@
+package xyz.catequest.spring.config.data;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingEntityListenerConfig {}
